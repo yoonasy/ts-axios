de1bdbd41b5800d37156da2b791720f1b94a4ee0
js52 <js52@js52.c>
2019-10-01 00:41:09 +0800

    [build] 0.1.5

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -124,7 +124,12 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
             request.abort()
             reject(reason)
           })
-          .catch()
+          .catch(
+            /* istanbul ignore next */
+            () => {
+              // do nothing
+            }
+          )
       }
     }
 
